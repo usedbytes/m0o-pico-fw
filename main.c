60ad057f7cbe2f6de8a90f4d1eb6706e3dcb8bbc
Brian Starkey <stark3y@gmail.com>
2021-10-21 23:08:17 +0100

    WIP: Hack in logging
    It's working, so commit in this state

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
+#include "pico/mutex.h"
 #include "hardware/dma.h"
 #include "hardware/uart.h"
 #include "hardware/watchdog.h"
@@ -19,11 +20,16 @@
 #define UART_BAUD   921600
 
 #define CMD_SYNC  (('S' << 0) | ('Y' << 8) | ('N' << 16) | ('C' << 24))
+#define CMD_LOGS  (('L' << 0) | ('O' << 8) | ('G' << 16) | ('S' << 24))
 #define RSP_SYNC (('P' << 0) | ('I' << 8) | ('C' << 16) | ('O' << 24))
 #define RSP_OK   (('O' << 0) | ('K' << 8) | ('O' << 16) | ('K' << 24))
 #define RSP_ERR  (('E' << 0) | ('R' << 8) | ('R' << 16) | ('!' << 24))
 
+struct log_buffer logger;
+
 static uint32_t handle_sync(uint32_t *args_in, uint8_t *data_in, uint32_t *resp_args_out, uint8_t *resp_data_out);
+static uint32_t size_logs(uint32_t *args_in, uint32_t *data_len_out, uint32_t *resp_data_len_out);
+static uint32_t handle_logs(uint32_t *args_in, uint8_t *data_in, uint32_t *resp_args_out, uint8_t *resp_data_out);
 
 struct command_desc {
 	uint32_t opcode;
@@ -40,6 +46,13 @@ const struct command_desc cmds[] = {
 		.resp_nargs = 0,
 		.size = NULL,
 		.handle = &handle_sync,
+	},
+	{
+		.opcode = CMD_LOGS,
+		.nargs = 0,
+		.resp_nargs = 1,
+		.size = &size_logs,
+		.handle = &handle_logs,
 	},
 };
 
@@ -207,38 +220,227 @@ static enum state state_error(struct cmd_context *ctx)
 	return STATE_WAIT_FOR_SYNC;
 }
 
+#define LOG_BUFFER_SIZE 1024
+
+struct log_message {
+	uint32_t timestamp;
+	uint8_t len;
+	// char string[];
+} __attribute__ ((packed));
+
+struct log_buffer {
+	uint8_t buf[LOG_BUFFER_SIZE];
+	mutex_t lock;
+	volatile uint16_t insert_idx;
+	volatile uint16_t extract_idx;
+	volatile uint16_t space;
+} __attribute__ ((aligned (LOG_BUFFER_SIZE)));
+
+void log_init(struct log_buffer *log)
+{
+	mutex_init(&log->lock);
+	memset(log->buf, 0, sizeof(log->buf));
+	log->space = sizeof(log->buf);
+	log->insert_idx = 0;
+	log->extract_idx = 0;
+}
+
+static inline void __log_write_bytes(struct log_buffer *log, const uint8_t *data, uint16_t len)
+{
+	uint16_t insert_idx = log->insert_idx;
+	uint16_t space_before_wrap = sizeof(log->buf) - insert_idx;
+	uint16_t first_len = space_before_wrap < len ? space_before_wrap : len;
+
+	memcpy(log->buf + insert_idx, data, first_len);
+	memcpy(log->buf, data + first_len, len - first_len);
+
+	log->insert_idx = (insert_idx + len) % sizeof(log->buf);
+	log->space -= len;
+}
+
+static inline void __log_peek_bytes(struct log_buffer *log, uint8_t *data, uint16_t len)
+{
+	uint16_t extract_idx = log->extract_idx;
+	uint16_t space_before_wrap = sizeof(log->buf) - extract_idx;
+	uint16_t first_len = space_before_wrap < len ? space_before_wrap : len;
+
+	memcpy(data, log->buf + extract_idx, first_len);
+	memcpy(data + first_len, log->buf, len - first_len);
+}
+
+static inline void __log_read_bytes(struct log_buffer *log, uint8_t *data, uint16_t len)
+{
+	uint16_t extract_idx = log->extract_idx;
+	uint16_t space_before_wrap = sizeof(log->buf) - extract_idx;
+	uint16_t first_len = space_before_wrap < len ? space_before_wrap : len;
+
+	memcpy(data, log->buf + extract_idx, first_len);
+	memcpy(data + first_len, log->buf, len - first_len);
+
+	log->extract_idx = (extract_idx + len) % sizeof(log->buf);
+	log->space += len;
+}
+
+static inline void log_make_space(struct log_buffer *log, uint16_t at_least)
+{
+	int space = log->space;
+	int extract_idx = log->extract_idx;
+
+	while (space < at_least) {
+		struct log_message msg;
+		uint16_t len = sizeof(msg);
+		uint16_t space_before_wrap = sizeof(log->buf) - extract_idx;
+		uint16_t first_len = space_before_wrap < len ? space_before_wrap : len;
+
+		memcpy((uint8_t *)&msg, log->buf + extract_idx, first_len);
+		memcpy((uint8_t *)&msg + first_len, log->buf, len - first_len);
+
+		uint16_t msg_size = sizeof(msg) + msg.len;
+
+		extract_idx = (extract_idx + msg_size) % sizeof(log->buf);
+		space += msg_size;
+	}
+	log->extract_idx = extract_idx;
+	log->space = space;
+}
+
+void log_write(struct log_buffer *log, const char *message, uint8_t len)
+{
+	int space_reqd = sizeof(struct log_message) + len;
+
+	// Will never fit.
+	// TODO: Drop or truncate?
+	if (space_reqd >= (sizeof(log->buf) - sizeof(struct log_message))) {
+		return;
+	}
+
+	mutex_enter_blocking(&log->lock);
+
+	log_make_space(log, space_reqd);
+
+	struct log_message msg = {
+		.timestamp = time_us_32(),
+		.len = len,
+	};
+
+	__log_write_bytes(log, (const uint8_t *)&msg, sizeof(msg));
+	__log_write_bytes(log, (const uint8_t *)message, len);
+
+	mutex_exit(&log->lock);
+
+	// unlock
+}
+
+uint16_t log_drain(struct log_buffer *log, uint8_t *buf, uint16_t size)
+{
+	mutex_enter_blocking(&log->lock);
+
+	uint16_t extract_idx = log->extract_idx;
+	uint16_t insert_idx = log->insert_idx;
+	uint16_t used = 0;
+
+	while (extract_idx != insert_idx) {
+		struct log_message msg;
+		uint16_t len = sizeof(msg);
+		uint16_t space_before_wrap = sizeof(log->buf) - extract_idx;
+		uint16_t first_len = space_before_wrap < len ? space_before_wrap : len;
+
+		memcpy((uint8_t *)&msg, log->buf + extract_idx, first_len);
+		memcpy((uint8_t *)&msg + first_len, log->buf, len - first_len);
+
+		uint16_t msg_size = sizeof(msg) + msg.len;
+		if (used + msg_size >= size) {
+			break;
+		}
+
+		__log_read_bytes(log, buf + used, msg_size);
+		used += msg_size;
+		extract_idx = (extract_idx + msg_size) % sizeof(log->buf);
+	}
+
+	mutex_exit(&log->lock);
+
+	return used;
+}
+
+static uint32_t size_logs(uint32_t *args_in, uint32_t *data_len_out, uint32_t *resp_data_len_out)
+{
+	*data_len_out = 0;
+	*resp_data_len_out = MAX_DATA_LEN;
+
+	return RSP_OK;
+}
+
+static uint32_t handle_logs(uint32_t *args_in, uint8_t *data_in, uint32_t *resp_args_out, uint8_t *resp_data_out)
+{
+	uint16_t drained = log_drain(&logger, resp_data_out, MAX_DATA_LEN);
+	resp_args_out[0] = drained;
+
+	return RSP_OK;
+}
+
 int main() {
 	uart_init(uart0, UART_BAUD);
 	gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
 	gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 	uart_set_hw_flow(uart0, false, false);
 
+	log_init(&logger);
+
+	/*
+	uint8_t tmp[1024];
+	uint16_t drained;
+
+	while (1) {
+		uart_write_blocking(uart0, "BLAH BLAH\r\n", strlen("BLAH BLAH\r\n"));
+		log_write(&logger, "Hello", strlen("Hello"));
+		log_write(&logger, "World", strlen("World"));
+
+		drained = log_drain(&logger, tmp, sizeof(tmp));
+
+		uart_write_blocking(uart0, tmp, drained);
+
+		sleep_ms(1000);
+	}
+	*/
+
 	struct cmd_context ctx;
-	uint8_t uart_buf[sizeof(uint32_t) * (1 + MAX_NARG + MAX_DATA_LEN)];
+	uint8_t uart_buf[(sizeof(uint32_t) * (1 + MAX_NARG)) + MAX_DATA_LEN];
 	ctx.uart_buf = uart_buf;
 	enum state state = STATE_WAIT_FOR_SYNC;
 
 	while (1) {
 		switch (state) {
 		case STATE_WAIT_FOR_SYNC:
+			log_write(&logger, "waiting for sync", strlen("waiting for sync"));
 			state = state_wait_for_sync(&ctx);
+			log_write(&logger, "done waiting", strlen("done waiting"));
 			break;
 		case STATE_READ_OPCODE:
+			log_write(&logger, "read opcode", strlen("read opcode"));
 			state = state_read_opcode(&ctx);
+			log_write(&logger, "done opcode", strlen("done opcode"));
 			break;
 		case STATE_READ_ARGS:
+			log_write(&logger, "read args", strlen("read args"));
 			state = state_read_args(&ctx);
+			log_write(&logger, "done args", strlen("done args"));
 			break;
 		case STATE_READ_DATA:
+			log_write(&logger, "read data", strlen("read data"));
 			state = state_read_data(&ctx);
+			log_write(&logger, "done data", strlen("done data"));
 			break;
 		case STATE_HANDLE_DATA:
+			log_write(&logger, "handle data", strlen("handle data"));
 			state = state_handle_data(&ctx);
+			log_write(&logger, "done handle", strlen("done handle"));
 			break;
 		case STATE_ERROR:
+			log_write(&logger, "do error", strlen("do error"));
 			state = state_error(&ctx);
+			log_write(&logger, "done error", strlen("done error"));
 			break;
 		}
 	}
-
 }
