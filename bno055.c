ea1ec052dfa90a9574a43e18ca998871535d9afe
Brian Starkey <stark3y@gmail.com>
2022-01-05 10:29:24 +0000

    bno055: Whitespace

--- bno055.c
+++ bno055.c
@@ -2,20 +2,20 @@
 // SPDX-License-Identifier: MIT
 //
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2015 Gaurav Hirlekar
 // Copyright (c) 2021 Brian Starkey <stark3y@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -54,7 +54,7 @@ static int bno055_set_page(struct bno055 *bno055, uint8_t page)
 
 	return 0;
 }
- 
+
 int bno055_init(struct bno055 *bno055, i2c_inst_t *i2c, uint8_t addr)
 {
 	int ret;
